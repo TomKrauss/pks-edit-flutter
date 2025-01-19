0c3c123b2b8b25f2ad70243b77577e2bf1e9c5ba
Tom Krauss <tom.krauss@gebit.de>
2025-01-19 12:18:57 +0100

    NO-ISSUE: Linux kludge.

--- lib/bloc/editor_bloc.dart
+++ lib/bloc/editor_bloc.dart
@@ -747,7 +747,12 @@ class EditorBloc {
         minimumSize: p.show == MainWindowPlacement.swShowMaximized ? null : size,
         skipTaskbar: false);
     await windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.setPosition(Offset(p.left * wFactor, p.top*hFactor));
+      if (Platform.isLinux) {
+        await windowManager.center();
+      } else {
+        await windowManager.setPosition(
+            Offset(p.left * wFactor, p.top * hFactor));
+      }
       await windowManager.focus();
     });
     if (p.show == MainWindowPlacement.swShowMaximized) {
